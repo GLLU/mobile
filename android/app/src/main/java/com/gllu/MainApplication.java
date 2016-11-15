fa8d90e8503b1008c80d16b384b5bcc5cc8516e1
Udi Mosayev <udi@sheerci.com>
2016-11-15 13:05:29 +0200

    Added Facebook SDK to Android successfully

--- android/app/src/main/java/com/gllu/MainApplication.java
+++ android/app/src/main/java/com/gllu/MainApplication.java
@@ -1,5 +1,11 @@
 package com.gllu;
 
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
+
 import android.app.Application;
 import android.util.Log;
 
@@ -12,24 +18,42 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends Application
+        implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private static CallbackManager mCallbackManager =
+            CallbackManager.Factory.create();
+
+    protected static CallbackManager getCallbackManager() {
+        return mCallbackManager;
+    }
+
+    private final ReactNativeHost mReactNativeHost =
+            new ReactNativeHost(this) {
+
+                @Override
+                protected boolean getUseDeveloperSupport() {
+                    return BuildConfig.DEBUG;
+                }
+
+                @Override
+                protected List<ReactPackage> getPackages() {
+                    return Arrays.<ReactPackage>asList(
+                            new MainReactPackage(),
+                            new FBSDKPackage(mCallbackManager)
+                    );
+                }
+            };
+
     @Override
-    protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
-  }
 }
