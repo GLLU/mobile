5e00de97207f0293f4dd50acca0b8967167f0429
Udi Mosayev <udi@sheerci.com>
2016-11-17 00:14:15 +0200

    Sending FB Login data to server

--- views/LoginView.js
+++ views/LoginView.js
@@ -19,15 +19,39 @@ class LoginView extends Component {
   constructor(props) {
     super(props);
     this.state = {loginClicked: false};
+    this.loginSuccessful = this.loginSuccessful.bind(this)
   }
 
   loginSuccessful(data) {
-    alert(data.accessToken);
+    this.setState({accessToken: data["accessToken"], expirationTime: data["expirationTime"]})
+
+    fetch("http://api.gllu.dev/v1/login/facebook_sign_in.json",
+      {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({access_token: data["accessToken"]}),
+      })
+      .then((response) => response.json())
+      .then((responseData) => {
+        if(responseData){
+          this.setState({
+            responseData: responseData,
+          });
+        }
+      })
+      .catch((error) => {
+        console.warn(error);
+      })
+      .done();
   }
 
   render() {
     return(
       <View>
+        <Text>
+          Access Token: {this.state.accessToken},
+          Expiration Time: {this.state.expirationTime}
+        </Text>
         <LoginButton
           readPermissions={["email", "public_profile"]}
           onLoginFinished={
@@ -45,7 +69,9 @@ class LoginView extends Component {
               }
             }
           }
-          onLogoutFinished={() => alert("logout.")}/>
+          onLogoutFinished={() =>
+            this.setState({accessToken: '', expirationTime: ''})
+          }/>
       </View>
     )
   }
