2eb108f6b901f9a10eb89d3134e08b8723de1eec
Yonatan Itzhaky <yonatan@robin.co.il>
2017-01-16 13:12:49 +0200

    fix bodytype images that gone wide

--- js/components/myBodyType/horizontalCarousel/carouselItem.js
+++ js/components/myBodyType/horizontalCarousel/carouselItem.js
@@ -15,7 +15,7 @@ export default class CarouselItem extends Component {
     const {item, itemActive} = this.props;
     return (
       <View style={styles.imagePlaceHolder}>
-        <Image style={{flex: 1, width: w*.325, height: h*.5, resizeMode: 'stretch', marginRight: w*.125 , alignItems:'center', justifyContent: 'center'}}
+        <Image style={{flex: 1, width: w*.350, height: h*.5, resizeMode: 'contain', marginRight: w*.125 , alignItems:'center', justifyContent: 'center'}}
           source={item.imageUrl}>
             <Image source={itemActive ? item.shapeActive : item.shapeDeactive} style={{width:50, height: 80, resizeMode: 'stretch', bottom: 50}}></Image>
           </Image>
