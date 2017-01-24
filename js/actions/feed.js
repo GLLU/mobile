076195fb7ce3c18e357968159b1f96f3d05ec8c1
Yonatan Itzhaky <yonatan@robin.co.il>
2017-01-24 14:59:24 +0200

    refactor query for getFeed to make it more readable

--- js/actions/feed.js
+++ js/actions/feed.js
@@ -5,8 +5,11 @@ import { readEndpoint } from 'redux-json-api';
 export const SET_FEED_DATA = 'SET_FEED_DATA';
 
 export function getFeed(feedType,feedCategory='',feedTerm=''):Action {
+  const feedTypeQuery = `feed[type]=`+feedType;
+  const feedCategoryQuery = `&feed[category]=`+feedCategory
+  const feedTermQuery = `&feed[term]=`+feedTerm;
     return (dispatch) => {
-        return dispatch(readEndpoint(`feed?feed[type]=`+feedType+`&feed[category]=`+feedCategory+`&feed[term]=`+feedTerm+``)).then((feedData) => {
+        return dispatch(readEndpoint(`feed?`+feedTypeQuery+feedCategoryQuery+feedTermQuery+``)).then((feedData) => {
             console.log('feeds data', feedData);
             dispatch(setFeedData(feedData.data));
         });
