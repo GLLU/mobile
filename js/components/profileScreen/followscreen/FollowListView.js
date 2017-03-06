d79b054c55c04c0565be15661dcd6bd34bc46aff
Shir Brass <shirbr510@gmail.com>
2017-03-06 12:14:20 +0200

    Added implementation of FollowRow and added dummy data.

--- js/components/profileScreen/followscreen/FollowListView.js
+++ js/components/profileScreen/followscreen/FollowListView.js
@@ -4,12 +4,17 @@ import React, { Component } from 'react';
 import { ListView, Image, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import { View } from 'native-base';
 
-import FollowView from '../FollowView'
+import FollowRow from './FollowRow'
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: 20,
+    },
+    separator: {
+        flex: 1,
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: '#e6e6e6',
     },
 });
 
@@ -24,7 +29,22 @@ class FollowListView extends Component {
 
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
-            dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+            dataSource: ds.cloneWithRows(
+                [
+                    {
+                        name:"Alex Alexey",
+                        username:"alexAlex",
+                        aboutMe:"I'm so awesome omg",
+                        avatar:"https://marketplace.canva.com/MACIsTc3Y5M/1/thumbnail_large/canva-woman-avatar-icon-MACIsTc3Y5M.png",
+                        isFollowing:false
+                    },
+                    {
+                        name:"Israel Israeli",
+                        username:"israelisreal",
+                        aboutMe:"I'm with alex",
+                        avatar:"http://www.designshock.com/wp-content/uploads/2016/04/man-17-200.jpg",
+                        isFollowing:true
+                    },]),
         };
     }
 
@@ -33,7 +53,8 @@ class FollowListView extends Component {
             <ListView
                 style={styles.container}
                 dataSource={this.state.dataSource}
-                renderRow={(data) => <View><Text>{data}</Text></View>}
+                renderRow={(data) => <FollowRow {...data}/>}
+                renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator}/>}
             />
         );
     }
---
+++ js/components/profileScreen/followscreen/FollowRow.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+
+import FollowView from '../FollowView'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 12,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    textContainer: {
+        flex:7,
+        flexDirection:'column',
+        marginLeft: 12
+    },
+    followTitle: {
+        flex:0.5,
+        flexDirection:'row',
+    },
+    followUsername:{
+        color:'#00a9ff',
+        marginLeft:12
+    },
+    followAboutMe: {
+        flex:0.5,
+    },
+    photoContainer: {
+        flex:2,
+    },
+    photo: {
+        flex:1,
+        // //wierd responsive bug :D
+        // width:null,
+        // height:null,
+        width:50,
+        height:50,
+        borderRadius:25
+    },
+    followView: {
+        flex:3
+    }
+});
+
+class FollowRow extends Component {
+
+    constructor(props){
+        super(props);
+        this.renderFollowText=this.renderFollowText.bind(this);
+    }
+
+    renderFollowText(){
+        return(
+            <View style={styles.textContainer}>
+                <View style={styles.followTitle}>
+                    <Text >{this.props.name}</Text>
+                    <Text style={styles.followUsername}>@{this.props.username}</Text>
+                </View>
+                <Text style={styles.followAboutMe}>{this.props.aboutMe}</Text>
+            </View>
+        )};
+
+    render(){
+        return(
+            <View style={[styles.container,this.props.style]}>
+                <View style={styles.photoContainer}>
+                    <Image resizeMode='cover' source={{ uri: this.props.avatar.url}} style={styles.photo} />
+                </View>
+                {this.renderFollowText()}
+                <FollowView style={styles.followView} user={{id:this.props.userid, isFollowing:this.props.isFollowing}}/>
+            </View>
+        )};
+}
+
+export default FollowRow;
