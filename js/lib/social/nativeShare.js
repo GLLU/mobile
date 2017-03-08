e92cd2484dc8c0c3c83f16f50d36d3c945ef06a8
Shir Brass <shirbr510@users.noreply.github.com>
2017-03-08 15:00:24 +0200

    Feature/share (#91)
    * Implemented global share
    
    * re-arranged social lib and applied changes in usages of share
    
    * Removed facebook Webview for better look
    
    * dealt with Udi’s OCD :)

---
+++ js/lib/social/nativeShare.js
@@ -0,0 +1,27 @@
+import {Share} from 'react-native';
+
+function _shareTextNative(shareData) {
+    Share.share({
+        message: `${ shareData.text } ${ shareData.url }`,
+        url: shareData.url,
+        title: 'GLLU'
+    }, {
+        dialogTitle: 'Share GLLU',
+        excludedActivityTypes: [ /*here we can exclude sharing options on ios*/ ]
+    })
+        .then(this._logResult)
+        .catch((error) => console.log(`error: ${ error.message }`));
+}
+
+function _logResult(result) {
+    if (result.action === Share.sharedAction) {
+        console.log(`shared with an activityType: ${result.activityType}`)
+    }
+    else {
+        if (result.action === Share.dismissedAction) {
+            console.log('dismissed');
+        }
+    }
+}
+
+export default _shareTextNative;
