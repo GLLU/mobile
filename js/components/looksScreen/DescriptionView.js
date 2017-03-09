f765ec1e2fb8e95edb7ba49728b8220b093e274f
Shir Brass <shirbr510@gmail.com>
2017-03-09 18:08:42 +0200

    Added SpringAnimation to DescriptionView

--- js/components/looksScreen/DescriptionView.js
+++ js/components/looksScreen/DescriptionView.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Animated, View, Text, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,6 +19,9 @@ export default class DescriptionView extends Component {
 
   constructor(props) {
     super(props);
+    this.state= {
+      fadeAnimContent : new Animated.Value(-300)
+    };
   }
 
   static propTypes = {
@@ -30,16 +33,44 @@ export default class DescriptionView extends Component {
     style:{}
   };
 
+  componentDidMount(){
+    this._animateShow()
+  }
+
+  componentWillUnmount(){
+    this._animateHide()
+  }
+
+  _animateShow(){
+    Animated.spring(          // Uses easing functions
+      this.state.fadeAnimContent,    // The value to drive
+      {
+        toValue: 0,
+        friction: 9
+      }            // Configuration
+    ).start();
+  }
+
+  _animateHide(){
+    Animated.spring(          // Uses easing functions
+      this.state.fadeAnimContent,    // The value to drive
+      {
+        toValue: -300,
+        friction: 9
+      }            // Configuration
+    ).start();
+  }
+
   render() {
     return(
-      <View style={[this.props.style,styles.container]}>
+      <Animated.View style={[{bottom: this.state.fadeAnimContent},this.props.style,styles.container]}>
         <View>
           <Text style={styles.descriptionStyle}>
             {this.props.description + 'sdfjklf kjhafj sadhfjk hfjk hasjk hajksdh jkasdhf jkdshfjk hdsjkfh ajksdfhjk asf'}
           </Text>
         </View>
         <View style={{height:70}}/>
-      </View>
+      </Animated.View>
     );
   }
 }
--- js/components/looksScreen/styles.js
+++ js/components/looksScreen/styles.js
@@ -125,7 +125,7 @@ export default StyleSheet.create({
   },
   descriptionView: {
     position:'absolute',
-    bottom:0,
+    //bottom:0,
     left:0,
     right:0
   }
