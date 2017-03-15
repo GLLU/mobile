60240eda246c155ad474b99f77ad132d68b15451
Udi Mosayev <udi@sheerci.com>
2017-03-15 12:14:15 +0200

    Changed the hard-coded URL

--- js/components/login/LoginView.js
+++ js/components/login/LoginView.js
@@ -23,8 +23,7 @@ class LoginView extends Component {
     var jsonData = {access_token: data["accessToken"], expirationTime: data["expirationTime"]}
     this.setState(jsonData)
 
-    // why is this hardcoded? :(
-    fetch("https://api.gllu.com/v1/login/facebook_sign_in.json",
+    fetch(`${Config.API_URL}/login/facebook_sign_in.json`,
       {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
