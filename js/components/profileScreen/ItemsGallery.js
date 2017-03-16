b533d1fa0461f528e5b175f835d691efd22bb93f
Shir Brass <shirbr510@users.noreply.github.com>
2017-03-16 11:46:21 +0200

    Hotfix/items gallery overflow (#122)
    * fixed sizing for iOS
    
    * Adjusted threshold to points instead of pixels

--- js/components/profileScreen/ItemsGallery.js
+++ js/components/profileScreen/ItemsGallery.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { Image, TouchableOpacity, Text } from 'react-native';
+import { Image, TouchableOpacity, Text, Dimensions } from 'react-native';
 import { View } from 'native-base';
 
 import styles from './styles';
@@ -24,7 +24,7 @@ class ItemsGallery extends Component {
   }
 
   renderAddItemBtn() {
-    if(this.props.isMyProfile) {
+    if (this.props.isMyProfile) {
       return (
         <TouchableOpacity style={styles.addItemContainer} onPress={(e) => this.props.addNewItem()}>
           <Image source={require('../../../images/icons/plus.png')} style={[styles.itemPic, styles.addItem]}/>
@@ -33,15 +33,27 @@ class ItemsGallery extends Component {
     }
   }
 
+  getGalleryItemsCount(screenWidthInPoints,isMyProfile){
+    let count = 4;
+    if(screenWidthInPoints <= 320) {
+      count -= 1
+    }
+    if(isMyProfile){
+      count-=1;
+    }
+    return count;
+  }
+
   getGalleryItems() {
-    let limiter = this.props.isMyProfile ? 3 : 4;
-    let flatLooksArr = []
+    const {width} = Dimensions.get('window');
+    const limiter = this.getGalleryItemsCount(width,this.props.isMyProfile);
+    let flatLooksArr = [];
     return (
       this.props.latest_looks.map((look, index) => {
-        if(index < limiter){
+        if (index < limiter) {
           let thumbImage = _.find(look.cover, image => image.version == 'thumb');
           let cover = _.find(look.cover, image => image.version == 'medium')
-          flatLooksArr.push(  {
+          flatLooksArr.push({
             liked: look.is_liked,
             type: look.user_size.body_type,
             id: look.id,
@@ -60,7 +72,7 @@ class ItemsGallery extends Component {
           });
           return (
             <TouchableOpacity onPress={(e) => this.props.itemPress(flatLooksArr[index])} key={index}>
-              <Image  source={{uri: thumbImage.url}} style={styles.itemPic} />
+              <Image source={{uri: thumbImage.url}} style={styles.itemPic}/>
             </TouchableOpacity>
           )
         }
@@ -73,8 +85,8 @@ class ItemsGallery extends Component {
       <View style={styles.itemsContainer}>
         <View style={styles.itemsSeparator}>
           <TouchableOpacity onPress={(e) => this.props.itemsPress()} style={styles.itemsTotal}>
-              <Text style={[styles.text, styles.number]}>{this.props.looksCount}</Text>
-              <Text style={styles.text}>Items</Text>
+            <Text style={[styles.text, styles.number]}>{this.props.looksCount}</Text>
+            <Text style={styles.text}>Items</Text>
           </TouchableOpacity>
           <View style={styles.itemsRow}>
             {this.getGalleryItems()}
