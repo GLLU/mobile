886995f010cb85228aed2d69a850a5da2cf6ec57
yonatan itzhaky <yonatan@gllu.com>
2017-05-17 15:18:08 +0300

    cleanup

--- js/actions/user.js
+++ js/actions/user.js
@@ -193,10 +193,8 @@ const signUp = function(dispatch, data) {
       const body = {user: data};
       dispatch(rest.actions.users.post({}, { body: JSON.stringify(body) }, (err, data) => {
         if (err) {
-          console.log('err',err)
           reject(err);
         } else {
-          console.log('goodsinguo')
           resolve(data);
         }
       }));
@@ -207,36 +205,31 @@ const signUp = function(dispatch, data) {
 export function emailSignUp(data):Action {
   return (dispatch, getState) => {
     dispatch(hideFatalError());
-    if(!_.isEmpty(data)) {
-      console.log('blabbb1',data)
+    if(data) {
       signUp(dispatch, data).then(data => {
         const user = getState().user;
         const invitation_is_used = user.invitation_is_used;
         const invitation_token = user.invitation_token;
         signInFromRest(dispatch, data, invitation_token, invitation_is_used);
       }).catch(err => {
-        console.log('blabbb')
-
-      });
-    } else {
-      console.log('data',data)
-      if (err.errors && err.errors.length > 0) {
-        const pointers = [];
-        let errorString = '';
-        err.errors.map((error) => {
-          pointers.push( _.capitalize(_.last(_.split(error.source.pointer, '/'))));
-        });
-        if(pointers.length === 1){
-          dispatch(showFatalError(pointers[0]+' has already taken'))
-        } else {
-          for(let i = 0; i<pointers.length-1; i++) {
-            errorString += pointers[i]+' & ';
+        if (err.errors && err.errors.length > 0) {
+          const pointers = [];
+          let errorString = '';
+          err.errors.map((error) => {
+            pointers.push( _.capitalize(_.last(_.split(error.source.pointer, '/'))));
+          });
+          if(pointers.length === 1){
+            dispatch(showFatalError(pointers[0]+' has already taken'))
+          } else {
+            for(let i = 0; i<pointers.length-1; i++) {
+              errorString += pointers[i]+' & ';
+            }
+            dispatch(showFatalError(errorString+pointers[pointers.length-1]+' are already taken'))
           }
-          dispatch(showFatalError(errorString+pointers[pointers.length-1]+' are already taken'))
+        } else {
+          dispatch(showFatalError(`Unknown error: ${err}`));
         }
-      } else {
-        dispatch(showFatalError(`Unknown error: ${err}`));
-      }
+      });
     }
   };
 }
