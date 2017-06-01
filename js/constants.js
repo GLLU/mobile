c3a9a3611f7eb827ad27304a1fb231fe3a74e5bc
Shir Brass <shirbr510@users.noreply.github.com>
2017-06-01 17:25:51 +0300

    Feature/replace gllu links (#256)
    * Fixed linking from gllu.com to infash.com
    
    * fixed app_name in android  to be with capital F

--- js/constants.js
+++ js/constants.js
@@ -1,9 +1,9 @@
 export const SUPPORT_EMAIL = 'hello@gllu.com';
 export const EMAIL_URL = `mailto:${SUPPORT_EMAIL}`;
-export const TERMS_URL = 'https://www.gllu.com/terms';
-export const PRIVACY_URL = 'https://www.gllu.com/privacy';
-export const COPYRIGHT_URL = 'https://www.gllu.com/copyrights';
-export const RATE_US_URL = 'https://www.gllu.com/rate_us';
+export const TERMS_URL = 'https://www.infash.com/terms';
+export const PRIVACY_URL = 'https://www.infash.com/privacy';
+export const COPYRIGHT_URL = 'https://www.infash.com/copyrights';
+export const RATE_US_URL = 'https://www.infash.com/rate_us';
 export const LOOK_STATES = {
   DRAFT: 'draft',
   PUBLISHED: 'published'
