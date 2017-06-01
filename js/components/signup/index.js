aad601556f9406bf064ea918309e20863a091cae
yonatan itzhaky <yonatan@gllu.com>
2017-06-01 15:11:21 +0300

    small typo fix

--- js/components/signup/index.js
+++ js/components/signup/index.js
@@ -253,7 +253,7 @@ class SignUpPage extends BasePage {
 
             </Content>
             <View style={styles.bottomContainerContent}>
-              <Text style={styles.text}>By signing-up I agree to inFash's </Text>
+              <Text style={styles.text}>By signing-up I agree to inFash </Text>
               <Text style={styles.link} onPress={this.handleTermPress.bind(this)}>Terms</Text>
               <Text style={styles.text}> and </Text>
               <Text style={styles.link} onPress={this.handlePrivacyPolicyPress.bind(this)}>Privacy Policy</Text>
