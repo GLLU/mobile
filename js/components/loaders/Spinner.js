9ceefb3b47b1e613e60c1c77a2e1fbf61d8340b8
Shir Brass <shirbr510@gmail.com>
2017-06-04 14:56:38 +0300

    Remove usage of compute props

--- js/components/loaders/Spinner.js
+++ js/components/loaders/Spinner.js
@@ -1,40 +1,28 @@
 /* @flow */
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { ActivityIndicator } from 'react-native';
 import computeProps from 'native-base/src/Utils/computeProps';
 
 
 export default class Spinner extends Component {
 
-  prepareRootProps() {
-    const type = {
-      height: 80,
-    };
-
-    const defaultProps = {
-      style: type,
-    };
-
-    return computeProps(this.props, defaultProps);
-  }
-
+  getColor = () => {
+    if (this.props.color) {
+      return this.props.color;
+    }
+    return '#45D56E';
+  };
 
   render() {
-    const getColor = () => {
-      if (this.props.color) {
-        return this.props.color;
-      }
-      return '#45D56E';
-    };
-
     return (
       <ActivityIndicator
-        {...this.prepareRootProps()}
-        color={getColor()}
+        {...this.props}
+        style={ {height: 80}}
+        color={this.getColor()}
         size={this.props.size ? this.props.size : 'large'}
       />
-        );
+    );
   }
 
 }
