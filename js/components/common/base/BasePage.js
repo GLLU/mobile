16fca618624559216b27c21cdd5b42e611fc14ec
Shir Brass <shirbr510@gmail.com>
2017-06-12 16:52:25 +0300

    Split BasePage into inherited BasePage and navigationHOC

--- js/components/common/base/BasePage.js
+++ js/components/common/base/BasePage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Alert } from 'react-native';
 import BaseComponent from './BaseComponent';
-import {NavigationActions} from "react-navigation";
+import withNavigation from '../navigation/WithNavigation'
 
 class BasePage extends BaseComponent {
   constructor(props) {
@@ -12,55 +11,13 @@ class BasePage extends BaseComponent {
     this.resetWithPayload=this.resetWithPayload.bind(this);
   }
 
-  setLeaveBreadcrumb(props) {
-    this.bugsnag.leaveBreadcrumb("Page", {
-      type: 'navigation',
-      component: this.constructor.name,
-      props: props,
-    });
-  }
+  resetTo=this.props.resetTo;
 
-  resetTo(route) {
-    this.resetWithPayload({
-      index: 0,
-      actions: [NavigationActions.navigate({ routeName: route })]
-    });
-  }
+  resetWithPayload=this.props.resetWithPayload;
 
-  resetWithPayload(payload){
-    this.props.navigation.dispatch(new NavigationActions.reset(payload))
-  }
+  navigateTo=this.props.navigateTo;
 
-  navigateTo(route,params) {
-    this.props.navigation.navigate(route, params)
-  }
-
-  goBack(withConfirmation = false) {
-    this.logEvent(this.constructor.name, { name: `user pressed back`});
-    if (withConfirmation===true) {
-      Alert.alert(
-        '',
-        'Are you sure you want to go back?',
-        [
-          {
-            text: 'Cancel',
-            onPress: () => {
-              console.log('Cancel Pressed');
-            },
-            style: 'cancel'
-          },
-          {
-            text: 'OK',
-            onPress: () => {
-              this.goBack(false);
-            }
-          }
-        ]
-      );
-    } else {
-      this.props.navigation.goBack();
-    }
-  }
+  goBack=this.props.goBack;
 }
 
-export default BasePage;
+export default withNavigation(BasePage);
