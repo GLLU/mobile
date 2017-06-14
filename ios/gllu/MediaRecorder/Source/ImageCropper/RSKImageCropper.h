0eb4e1fa26a318e47cdd4c975b024799afc3172b
M G <mg@gmail.com>
2017-06-14 18:21:37 +0300

    integration

---
+++ ios/gllu/MediaRecorder/Source/ImageCropper/RSKImageCropper.h
@@ -0,0 +1,10 @@
+
+#import <Foundation/Foundation.h>
+#import "CGGeometry+RSKImageCropper.h"
+#import "RSKImageCropViewController.h"
+#import "RSKImageCropViewController+Protected.h"
+#import "RSKImageScrollView.h"
+#import "RSKInternalUtility.h"
+#import "RSKTouchView.h"
+#import "UIApplication+RSKImageCropper.h"
+#import "UIImage+RSKImageCropper.h"
---
+++ ios/gllu/MediaRecorder/Source/VideoTrimmer/VideoTrimmerViewController.h
@@ -0,0 +1,8 @@
+
+#import <UIKit/UIKit.h>
+
+@interface VideoTrimmerViewController : UIViewController
+
+- (instancetype)initWithAssetUrl:(NSURL*)url;
+
+@end
