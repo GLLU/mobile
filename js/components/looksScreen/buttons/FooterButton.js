51c4d91c24bed89c0643c6170992d56595b91b86
Shir Brass <shirbr510@gmail.com>
2017-07-02 12:59:25 +0300

    Footer Buttons are now round

--- js/components/looksScreen/buttons/FooterButton.js
+++ js/components/looksScreen/buttons/FooterButton.js
@@ -1,7 +1,35 @@
 import React, { PureComponent } from 'react';
-import { View, Image, TouchableHighlight, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import * as _ from 'lodash'
-import styles from '../styles'
+
+const styles = StyleSheet.create({
+  footerButton: {
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent:'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    height: 45,
+    width: 45,
+    borderRadius: 25,
+  },
+  footerButtonActive: {
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent:'center',
+    backgroundColor: 'rgba(0,215,178, 0.75)',
+    height: 45,
+    width: 45,
+    borderRadius: 25,
+  },
+  footerButtonIcon: {
+    height: 25,
+    width: 25,
+    resizeMode: 'contain',
+    alignSelf: 'center'
+  }
+});
 
 export default class FooterButton extends PureComponent {
   constructor(props) {
@@ -30,12 +58,11 @@ export default class FooterButton extends PureComponent {
 
   render() {
     return (
-      <TouchableHighlight style={{margin:5, width: 45, maxWidth: 55, alignSelf: 'flex-end'}} onPress={this._onPress}>
+      <TouchableOpacity style={{margin:5, width: 45, maxWidth: 55, alignSelf: 'flex-end'}} onPress={this._onPress}>
         <View style={this._getStyle(this.props.isActive)}>
           <Image source={this.props.icon ? this.props.icon : { uri: this.props.iconUrl } } style={styles.footerButtonIcon}/>
         </View>
-      </TouchableHighlight>
+      </TouchableOpacity>
     );
   }
 }
-
