003b2c1d5757182d2bc4b00f5a4a4c3aa5beb116
Shir Brass <shirbr510@users.noreply.github.com>
2017-07-05 16:05:09 +0300

    Feature/rate us link (#364)
    * removed deprecation from flurryAnalytics & bumped version to 2.1.0
    
    * Altered rate-us link on android

--- js/constants.js
+++ js/constants.js
@@ -1,9 +1,13 @@
+import { Platform } from 'react-native'
+
 export const SUPPORT_EMAIL = 'hello@gllu.com';
 export const EMAIL_URL = `mailto:${SUPPORT_EMAIL}`;
 export const TERMS_URL = 'https://www.infash.com/terms';
 export const PRIVACY_URL = 'https://www.infash.com/privacy';
 export const COPYRIGHT_URL = 'https://www.infash.com/copyrights';
-export const RATE_US_URL = 'https://www.infash.com/rate_us';
+export const RATE_US_URL = Platform.OS === 'ios' ?
+  'https://www.infash.com/rate_us' :
+  'https://play.google.com/store/apps/details?id=com.infash.release';
 export const LOOK_STATES = {
   DRAFT: 'draft',
   PUBLISHED: 'published'
