23e848ce12be7c7c8c4bbbe326bb507204a1fd2a
Shir Brass <shirbr510@gmail.com>
2017-07-16 17:12:23 +0300

    altered the structure of block method

--- js/components/looksScreen/menu/MenuAction.js
+++ js/components/looksScreen/menu/MenuAction.js
@@ -3,6 +3,9 @@ import { Animated, View, Text, StyleSheet, TouchableOpacity, Image, FlatList, Al
 import { noop } from 'lodash'
 import SolidButton from "../../common/buttons/SolidButton";
 import * as _ from "lodash";
+import i18n from 'react-native-i18n';
+import Colors from "../../../styles/Colors.styles";
+import Fonts from "../../../styles/Fonts.styles";
 
 const styles = StyleSheet.create({
   container: {
@@ -20,15 +23,27 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems:'center',
+    alignItems: 'center',
     paddingHorizontal: 20
   },
   message: {
     textAlign: 'center',
     fontSize: 16,
-    paddingHorizontal:10,
+    paddingHorizontal: 10,
     fontWeight: '500'
   },
+  confirmationButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  confirmationText: {
+    textAlign: 'center',
+    color: Colors.black,
+    fontFamily: Fonts.regularFont,
+    fontSize:20,
+    fontWeight: 'bold'
+  }
 });
 
 class MenuAction extends Component {
@@ -94,13 +109,23 @@ class MenuAction extends Component {
     );
   }
 
+  confirmationButton(label, onPress) {
+    return (
+      <TouchableOpacity style={styles.confirmationButton} onPress={onPress}>
+        <Text style={styles.confirmationText}>{label}</Text>
+      </TouchableOpacity>
+    )
+  }
+
   renderAreYouSure(areYouSureMessage) {
     return (
       <View style={{height: 75, paddingVertical: 15}}>
         <View style={styles.areYouSureContainer}>
-          <Button title='no' style={{flex:1}} onPress={this.onCancelPress}/>
-          <Text style={[styles.message,{flex:4}]}>{areYouSureMessage}</Text>
-          <Button title='yes' style={{flex:1}} onPress={this.onConfirmPress}/>
+          {this.confirmationButton(i18n.t('NO'), this.onCancelPress)}
+          {/*<Button title={i18n.t('NO')} style={{flex:1,color:Colors.black}} onPress={this.onCancelPress}/>*/}
+          <Text style={[styles.message, {flex: 4}]}>{areYouSureMessage}</Text>
+          {this.confirmationButton(i18n.t('YES'), this.onConfirmPress)}
+          {/*<Button title={i18n.t('YES')} style={{flex:1, color:Colors.primaryColor}} onPress={this.onConfirmPress}/>*/}
         </View>
       </View>
     );
