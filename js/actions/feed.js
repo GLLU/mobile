f5fb4fa5791ce070c8e12f926d92960012dce877
yonatan itzhaky <yonatan@gllu.com>
2017-07-19 12:04:07 +0300

    refactor look schemas to schemas js

--- js/actions/feed.js
+++ js/actions/feed.js
@@ -2,9 +2,9 @@
 
 import _ from 'lodash';
 import LooksService from '../services/looksService';
-import { normalize, schema } from 'normalizr';
+import { normalize, arrayOf } from 'normalizr';
 import { unifyLooks } from '../utils/FeedUtils';
-
+import { lookSchema, lookListSchema } from '../schemas/schemas'
 export const SET_FLAT_LOOKS_FEED_DATA = 'SET_FLAT_LOOKS_FEED_DATA';
 export const SET_FLAT_LOOKS_DATA = 'SET_FLAT_LOOKS_DATA';
 export const CLEAR_FEED_DATA = 'CLEAR_FEED_DATA';
@@ -27,11 +27,11 @@ export function getFeed(query, retryCount = 0) {
       },
     });
     return LooksService.getLooks({ ...query, 'page[size]': 10, 'page[number]': 1 }).then((data) => {
-      if (!_.isEmpty(data)) {
+      if (data) {
         const { looks, meta } = data;
-        const normalizedLooksData = normalizeLooksData(looks)
+        const normalizedLooksData = normalize(looks, [ lookSchema] );
         dispatch(setLooksData({ flatLooksData: normalizedLooksData.entities.looks, query: newState }));
-        dispatch(setFeedData({ flatLooksIdData: normalizedLooksData.result.looks, meta, query: newState }));
+        dispatch(setFeedData({ flatLooksIdData: normalizedLooksData.result, meta, query: newState }));
         dispatch(loadMore());
         Promise.resolve(data);
       } else if (retryCount < 5) {
@@ -43,16 +43,6 @@ export function getFeed(query, retryCount = 0) {
       Promise.reject(error);
     });
   };
-}
-
-
-export function normalizeLooksData(looks) {
-  const newData = {}
-  newData.looks = looks
-  const LooksData = new schema.Entity('looks');
-  const mySchema = { looks: [LooksData] };
-  const normalizedData = normalize(newData, mySchema);
-  return normalizedData;
 }
 
 export function clearFeed() {
@@ -73,12 +63,12 @@ export function loadMore(retryCount = 0) {
     const params = parseQueryFromState(newState);
     return new Promise((resolve, reject) => {
       return LooksService.getLooks(params).then((data) => {
-        if (!_.isEmpty(data)) {
+        if (data) {
           const { looks, meta } = data;
-          const normalizedLooksData = normalizeLooksData(looks)
+          const normalizedLooksData = normalize(looks, [ lookSchema] );
           const unfiedLooks = unifyLooks(normalizedLooksData.entities.looks, getState().looks.flatLooksData)
           dispatch(setLooksData({ flatLooksData: { ...unfiedLooks }, query: newState }));
-          const flatLooksIdData = state.flatLooksData.concat(normalizedLooksData.result.looks)
+          const flatLooksIdData = state.flatLooksData.concat(normalizedLooksData.result)
           dispatch(setFeedData({ flatLooksIdData, meta, query: newState }));
           resolve(data.looks);
         } else if (retryCount < 5) {
