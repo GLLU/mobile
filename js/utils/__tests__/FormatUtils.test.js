472e84d404479f0945e98d8626d1f45d1971c6c3
Shir Brass <shirbr510@users.noreply.github.com>
2017-07-20 11:05:44 +0300

    Feature/new number format rules (#407)
    * replaced format logic of formatNumberAsString to support precisioning
    
    * Implemented floorWithPrecision (including tests)

--- js/utils/__tests__/FormatUtils.test.js
+++ js/utils/__tests__/FormatUtils.test.js
@@ -1,8 +1,8 @@
 // @flow
 
-import { formatNumberAsString } from '../FormatUtils';
+import { floorWithPrecision, formatNumberAsString } from '../FormatUtils';
 
-describe('fomatNumberAsString util', () => {
+describe('formatNumberAsString util', () => {
 
   test('null', () => {
     expect(formatNumberAsString(null)).toBe('N/A');
@@ -45,11 +45,19 @@ describe('fomatNumberAsString util', () => {
   });
 
   test('4 digit number', () => {
-    expect(formatNumberAsString(1337)).toBe('1K');
+    expect(formatNumberAsString(1337)).toBe('1.3K');
+  });
+
+  test('4 digit number', () => {
+    expect(formatNumberAsString(2017)).toBe('2K');
   });
 
   test('5 digit number', () => {
-    expect(formatNumberAsString(11270)).toBe('11K');
+    expect(formatNumberAsString(11270)).toBe('11.2K');
+  });
+
+  test('5 digit number', () => {
+    expect(formatNumberAsString(52048)).toBe('52K');
   });
 
   test('6 digit number', () => {
@@ -59,10 +67,45 @@ describe('fomatNumberAsString util', () => {
   test('7 digit number', () => {
     expect(formatNumberAsString(6000000)).toBe('6M');
   });
+  test('7 digit number', () => {
+    expect(formatNumberAsString(7560000)).toBe('7.5M');
+  });
   test('8 digit number', () => {
     expect(formatNumberAsString(13371337)).toBe('13M');
   });
   test('9 digit number', () => {
     expect(formatNumberAsString(123456789)).toBe('123M');
   });
+});
+
+describe('floorWithPrecision util', () => {
+
+  test('negative number with 0 precision', () => {
+    expect(floorWithPrecision(-1024.349)).toBe(-1024);
+  });
+
+  test('negative number with 1 precision', () => {
+    expect(floorWithPrecision(-25490.992812738,1)).toBe(-25490.9);
+  });
+
+  test('negative number with 2 precision', () => {
+    expect(floorWithPrecision(-1440.134,2)).toBe(-1440.13);
+  });
+
+  test('zero with 2 precision', () => {
+    expect(floorWithPrecision(0,2)).toBe(0);
+  });
+
+  test('positive number with 0 precision', () => {
+    expect(floorWithPrecision(0.2048)).toBe(0);
+  });
+
+  test('positive number with 1 precision', () => {
+    expect(floorWithPrecision(124.816,1)).toBe(124.8);
+  });
+
+  test('positive number with 2 precision', () => {
+    expect(floorWithPrecision(123123.123,2)).toBe(123123.12);
+  });
+
 });
