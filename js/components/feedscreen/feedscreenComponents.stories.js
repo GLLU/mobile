2012ae21e53ea0b08156f84e5c3b8cb88881bef7
Shir Brass <shirbr510@gmail.com>
2017-07-20 11:51:49 +0300

    fixed stories to work with new props

--- js/components/feedscreen/feedscreenComponents.stories.js
+++ js/components/feedscreen/feedscreenComponents.stories.js
@@ -11,15 +11,15 @@ import FeedCommentsView from "./items/FeedCommentsView";
 storiesOf('feed screen components', module)
   .add('like view liked', () =>
     <View style={{backgroundColor: 'red', flex: 1, flexDirection: 'column'}}>
-      <FeedLikesView item={{likes:17982,liked:true}} onPress={action('like=pressed')} onLikesNumberPress={action('likes-number-pressed')}/>
+      <FeedLikesView likes={17982} isLiked={true} onPress={action('like=pressed')} onLikesNumberPress={action('likes-number-pressed')}/>
     </View>
   ).add('like view not liked', () =>
   <View style={{backgroundColor: 'red', flex: 1, flexDirection: 'column'}}>
-    <FeedLikesView item={{likes:17982,liked:false}} onPress={action('like=pressed')} onLikesNumberPress={action('likes-number-pressed')}/>
+    <FeedLikesView likes={76123} isLiked={false} onPress={action('like=pressed')} onLikesNumberPress={action('likes-number-pressed')}/>
   </View>
 ).add('like view no likes', () =>
   <View style={{backgroundColor: 'red', flex: 1, flexDirection: 'column'}}>
-    <FeedLikesView item={{likes:0,liked:false}} onPress={action('like=pressed')} onLikesNumberPress={action('likes-number-pressed')}/>
+    <FeedLikesView likes={0} isLiked={false} onPress={action('like=pressed')} onLikesNumberPress={action('likes-number-pressed')}/>
   </View>
 ).add('comment view with comments', () =>
   <View style={{backgroundColor: 'red', flex: 1, flexDirection: 'column'}}>
