1df07405249c4bbb436391dbc94430f1df983739
yonatan itzhaky <yonatan@gllu.com>
2017-08-13 14:58:51 +0300

    clean up

--- js/components/searchScreen/SectionSeparator.js
+++ js/components/searchScreen/SectionSeparator.js
@@ -3,42 +3,16 @@ import React, {Component} from 'react';
 import {Dimensions, TouchableOpacity, Text, View, StyleSheet} from 'react-native';
 import i18n from 'react-native-i18n';
 const deviceWidth = Dimensions.get('window').width;
-import Separator from '../common/lists/Separator';
-import UserActionRow from '../common/lists/UserActionRow';
 import Colors from '../../styles/Colors.styles';
 import {generateAdjustedSize} from '../../utils/AdjustabaleContent';
-import EmptyStateScreen from '../common/EmptyStateScreen';
 import Fonts from '../../styles/Fonts.styles';
-const emptyUser = require('../../../images/emptyStates/user-woman.png');
 const styles = StyleSheet.create({
-  tabContainer: {
-    backgroundColor: Colors.white,
-    flex: 1
-  },
+
   RowTitle: {
     color: Colors.gray,
     fontFamily: Fonts.regularFont,
     fontWeight: '300',
     fontSize: generateAdjustedSize(14),
-  },
-  historyRowContainer: {
-    height: generateAdjustedSize(60),
-    backgroundColor: Colors.white,
-    justifyContent: 'center',
-    borderBottomWidth: generateAdjustedSize(1),
-    borderBottomColor: Colors.separatorGray,
-  },
-  historyRowText: {
-    fontWeight: '600',
-    marginLeft: 15,
-    fontSize: generateAdjustedSize(14),
-  },
-  historyListContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: deviceWidth,
-    backgroundColor: Colors.backgroundGrey,
-    padding: generateAdjustedSize(15),
   },
   clearText: {
     color: 'red',
@@ -52,10 +26,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     width: deviceWidth,
     padding: generateAdjustedSize(15),
-  },
-  emptyViewContainer: {
-    flex: 1,
-    justifyContent: 'center',
   },
 });
 
