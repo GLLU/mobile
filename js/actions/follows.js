d7a17c803f1d03e2fc3a9f1054873ef5b615856d
yonatan itzhaky <yonatan@gllu.com>
2017-08-17 13:50:16 +0300

    removed unnecessary promise on follows

--- js/actions/follows.js
+++ js/actions/follows.js
@@ -7,50 +7,42 @@ export const SET_USER_FOLLOWS_DATA = 'SET_USER_FOLLOWS_DATA';
 export const START_FETCHING_FOLLOWING = 'following.START_FETCHING_FOLLOWING';
 export const INIT_USER_FOLLOWS = 'INIT_USER_FOLLOWS';
 import FollowsService from '../services/followsService';
-import {getFollowingFeed} from './feed';
+import { getFollowingFeed } from './feed';
 
 export function followUpdate(id) {
   return (dispatch, getState) => {
-    return new Promise((resolve) => {
-      FollowsService.follow(id).then(() => {
-        const query = getState().feed.following.query;
-        dispatch(getFollowingFeed(query));
-        resolve();
-      });
-    })
+    FollowsService.follow(id).then(() => {
+      const query = getState().feed.following.query;
+      dispatch(getFollowingFeed(query));
+    });
   };
 }
 
 export function unFollowUpdate(id) {
   return (dispatch, getState) => {
-    return new Promise((resolve) => {
-      FollowsService.unFollow(id).then(() => {
-        const query = getState().feed.following.query;
-        dispatch(getFollowingFeed(query));
-        resolve();
-      });
-    })
+    FollowsService.unFollow(id).then(() => {
+      const query = getState().feed.following.query;
+      dispatch(getFollowingFeed(query));
+    });
   };
-
 }
 
 export function initUserFollows(data) {
   return {
     type: INIT_USER_FOLLOWS,
-    payload: data
+    payload: data,
   };
 }
 
 export function setUserFollowsData(data) {
   return {
     type: SET_USER_FOLLOWS_DATA,
-    payload: data
+    payload: data,
   };
 }
 
 export function getUserFollowsData(id, pageNumber = 1, pageSize = 25) {
   return (dispatch) => {
-
     if (pageNumber === 1) {
       dispatch({ type: START_FETCHING_FOLLOWING });
     }
@@ -59,13 +51,13 @@ export function getUserFollowsData(id, pageNumber = 1, pageSize = 25) {
       user_id: id,
       page: {
         size: pageSize,
-        number: pageNumber
-      }
+        number: pageNumber,
+      },
     }, {}, (err, userFollowsData) => {
       if (!err && userFollowsData) {
-        let followsData = {
+        const followsData = {
           currId: id,
-          follows: userFollowsData.follows
+          follows: userFollowsData.follows,
         };
         dispatch(setUserFollowsData(followsData));
       }
