38e2faece202ebf8871e5d64a7c49ae57f70f16c
Yonatani <yonatanitzhaky@gmail.com>
2017-09-10 18:16:10 +0300

    Feature/video error screen (#480)
    * if video has error while downloading 10 times, will show a msg instead
    
    * cleanup
    
    * cleanup
    
    * cleanup after android check

--- js/components/common/media/VideoWithCaching.js
+++ js/components/common/media/VideoWithCaching.js
@@ -1,72 +1,93 @@
 import React, { Component } from 'react';
-import { Image, View, StyleSheet } from 'react-native';
+import { Image, View, StyleSheet, Text } from 'react-native';
 import Video from 'react-native-video';
-import cachedWrapper from './CachedComponentWrapper'
-import listenToAppState from '../eventListeners/AppStateListener'
-import IsOnScreenChecker from './IsOnScreenChecker'
-import { debounce } from "lodash";
-import Spinner from "../../loaders/Spinner";
+import cachedWrapper from './CachedComponentWrapper';
+import listenToAppState from '../eventListeners/AppStateListener';
+import IsOnScreenChecker from './IsOnScreenChecker';
+import { debounce } from 'lodash';
+import Spinner from '../../loaders/Spinner';
+import Colors from '../../../styles/Colors.styles';
+import Fonts from '../../../styles/Colors.styles';
+import { generateAdjustedSize } from '../../../utils/AdjustabaleContent';
+
+const lowConnectivity = require('../../../../images/indicators/videoError.png');
 
 const styles = StyleSheet.create({
-  loaderContainer:{
+  loaderContainer: {
     position: 'absolute',
     top: 0,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+  },
+  lowConnectivityContainer: {
+    flexDirection: 'column',
+    backgroundColor: Colors.primaryColor,
+  },
+  lowConnectivityImage: {
+    width: 50,
+    height: 50,
+    marginBottom: 10,
+  },
+  lowConnectivityText: {
+    fontSize: generateAdjustedSize(10),
+    textAlign: 'center',
+    fontFamily: Fonts.regularFont,
+  },
 });
 
 class VideoWithCaching extends Component {
 
   static propTypes={
-    isCaching:React.PropTypes.bool,
-    localUri:React.PropTypes.string,
-    source:React.PropTypes.object,
-    currentAppState:React.PropTypes.string,
-    isOnScreen:React.PropTypes.bool,
-    preview:React.PropTypes.string
+    isCaching: React.PropTypes.bool,
+    localUri: React.PropTypes.string,
+    source: React.PropTypes.object,
+    currentAppState: React.PropTypes.string,
+    isOnScreen: React.PropTypes.bool,
+    preview: React.PropTypes.string,
   }
 
   static defaultProps={
-    preview:''
+    preview: '',
   }
 
   constructor(props) {
     super(props);
-    this.onVideoStartsPlaying=this.onVideoStartsPlaying.bind(this);
-    this.onError=this.onError.bind(this);
+    this.onVideoStartsPlaying = this.onVideoStartsPlaying.bind(this);
+    this.onError = this.onError.bind(this);
     this.state = {
       repeat: true,
-      isPlaying:false
-    }
-    this.forceCacheAttempts=0;
+      isPlaying: false,
+      connectionError: false,
+    };
+    this.forceCacheAttempts = 0;
   }
 
-  componentWillReceiveProps(nextProps){
-    if(nextProps.currentAppState!==this.props.currentAppState){
-      if(this.props.currentAppState==='active'&&this._root){
-        this._root.seek(0)
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.currentAppState !== this.props.currentAppState) {
+      if (this.props.currentAppState === 'active' && this._root) {
+        this._root.seek(0);
       }
-      this.setState({repeat:nextProps.currentAppState==='active'})
+      this.setState({ repeat: nextProps.currentAppState === 'active' });
     }
-    if(nextProps.isCaching&&nextProps.isCaching!==this.props.isCaching){
-      this.setState({isPlaying:false});
+    if (nextProps.isCaching && nextProps.isCaching !== this.props.isCaching) {
+      this.setState({ isPlaying: false });
     }
   }
 
-  static formatSource = (localUri, source = {}) => ({...source, uri: localUri});
+  static formatSource = (localUri, source = {}) => ({ ...source, uri: localUri });
 
-  onVideoStartsPlaying = ()=>{
-      if(this.state.isPlaying!==true){
-        this.setState({isPlaying:true})
-      }
-
+  onVideoStartsPlaying = () => {
+    if (this.state.isPlaying !== true) {
+      this.setState({ isPlaying: true });
+    }
   };
 
-  onError(err){
-    if(this.forceCacheAttempts<5){
+  onError(err) {
+    if (this.forceCacheAttempts < 10) {
       this.forceCacheAttempts++;
       this.props.forceCache();
+    } else {
+      this.setState({ connectionError: true });
     }
   }
 
@@ -76,24 +97,38 @@ class VideoWithCaching extends Component {
     }
     if (this.props.preview) {
       return (
-        <Image source={{uri: this.props.preview}} style={[this.props.style, styles.loaderContainer]}>
-          <Spinner color='grey'/>
+        <Image source={{ uri: this.props.preview }} style={[this.props.style, styles.loaderContainer]}>
+          <Spinner color="grey" />
         </Image>
-      )
+      );
     }
     return (
       <View style={[this.props.style, styles.loaderContainer]}>
-        <Spinner color='grey'/>
+        <Spinner color="grey" />
       </View>
-    )
+    );
+  };
+
+  renderLowConnectivity = () => {
+    if (!this.state.connectionError) {
+      return null;
+    }
+    return (
+      <View style={[this.props.style, styles.loaderContainer, styles.lowConnectivityContainer]}>
+        <Image source={lowConnectivity} resizeMode={'stretch'} style={styles.lowConnectivityImage} />
+        <Text style={styles.lowConnectivityText}>There's something wrong with the video. wer'e working on making it better</Text>
+      </View>
+
+    );
   };
 
   renderVideo = () => {
-    const {source, localUri} = this.props;
+    const { source, localUri } = this.props;
     const formattedSource = VideoWithCaching.formatSource(localUri, source);
-    if (!this.props.isCaching && this.state.repeat && this.props.isOnScreen) {
-      return <Video {...this.props} source={formattedSource} onProgress={this.onVideoStartsPlaying} onError={this.onError}
-                    ref={component => this._root = component}/>
+    if (!this.state.connectionError && !this.props.isCaching && this.state.repeat && this.props.isOnScreen) {
+      return (<Video
+        {...this.props} source={formattedSource} onProgress={this.onVideoStartsPlaying} onError={this.onError}
+        ref={component => this._root = component} />);
     }
     return null;
   }
@@ -103,12 +138,12 @@ class VideoWithCaching extends Component {
       <View>
         {this.renderVideo()}
         {this.renderLoader()}
+        {this.renderLowConnectivity()}
       </View>
-    )
+    );
   }
 }
 
 const cache = cachedWrapper(props => props.source.uri);
 export default cache(IsOnScreenChecker(listenToAppState(VideoWithCaching)));
-
 
