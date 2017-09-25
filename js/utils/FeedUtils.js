e42ae2fd454a9a320c7e2a2b0c4b3a4e58d144ce
Shir Brass <shirbr510@gmail.com>
2017-09-25 15:30:18 +0300

    Added random id pushing logic

--- js/utils/FeedUtils.js
+++ js/utils/FeedUtils.js
@@ -1,5 +1,7 @@
 // @flow
 
+import * as _ from 'lodash';
+
 export function getLooksById(feedLooksIds: array, flatLooksData: object) {
   return _.map(feedLooksIds, (lookId, index) => {
     const look = flatLooksData[lookId];
@@ -7,6 +9,19 @@ export function getLooksById(feedLooksIds: array, flatLooksData: object) {
     return look;
   });
 }
+
+export const pushIdsRandomly = (originalIds: Array<number>, newIds: Array<number>) => {
+  const newArray = [];
+  const indexedIds = _.map(newIds, newId => ({id: newId, index: _.random(originalIds.length)}));
+  _.times(originalIds.length, (i) => {
+    const newId=_.find(indexedIds, indexedId => i === indexedId.index);
+    if(newId){
+      newArray.push(newId.id);
+    }
+    newArray.push(originalIds[i]);
+  });
+  return _.uniq(newArray);
+};
 
 export function unifyLooks(newLooksData, stateLooksData) {
   const flatLooksData = { ...stateLooksData, ...newLooksData };
