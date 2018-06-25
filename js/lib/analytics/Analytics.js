c1d5c803e6e7715e033b2b3951b6669ede9fc7ad
sivanw <sivan@infash.com>
2018-06-25 11:29:02 +0300

    fix the analytics build issue

--- js/lib/analytics/Analytics.js
+++ js/lib/analytics/Analytics.js
@@ -1,73 +1,73 @@
 import FlurryAnalytics from './FlurryAnalytics';
-import GoogleAnalytics from './GoogleAnalytics';
+// import GoogleAnalytics from './GoogleAnalytics';
 import FacebookAnalytics from './FacebookAnalytics';
 import MixPanelAnalytics from './MixPanelAnalytics';
 import { APP_LOADED_EVENT, PAGE_LOADED_EVENT } from './constants';
 
 class Analytics {
-  constructor() {
-    console.log('Init Analytics');
-    this.tools = this.setupAnalytics();
-  }
+    constructor() {
+        console.log('Init Analytics');
+        this.tools = this.setupAnalytics();
+    }
 
-  setupAnalytics() {
-    return [
-      new GoogleAnalytics(),
-      new FlurryAnalytics(),
-      new FacebookAnalytics(),
-      new MixPanelAnalytics(),
-    ];
-  }
+    setupAnalytics() {
+        return [
+            // new GoogleAnalytics(),
+            new FlurryAnalytics(),
+            new FacebookAnalytics(),
+            new MixPanelAnalytics(),
+        ];
+    }
 
-  setUser(user) {
-    this._loop((x) => {
-      x.setUser(user);
-    });
-  }
+    setUser(user) {
+        this._loop((x) => {
+            x.setUser(user);
+        });
+    }
 
-  trackAppLoaded(params = {}) {
-    this.logEvent(APP_LOADED_EVENT, params, true);
-  }
+    trackAppLoaded(params = {}) {
+        this.logEvent(APP_LOADED_EVENT, params, true);
+    }
 
-  endTrackAppLoaded(params = {}) {
-    this.endTimedEvent(APP_LOADED_EVENT, params);
-  }
+    endTrackAppLoaded(params = {}) {
+        this.endTimedEvent(APP_LOADED_EVENT, params);
+    }
 
-  trackScreen(params = {}) {
-    this._loop((x) => {
-      if (typeof x.trackScreen === 'function') {
-        x.trackScreen(PAGE_LOADED_EVENT, Object.assign({}, params));
-      }
-    });
-  }
+    trackScreen(params = {}) {
+        this._loop((x) => {
+            if (typeof x.trackScreen === 'function') {
+                x.trackScreen(PAGE_LOADED_EVENT, Object.assign({}, params));
+            }
+        });
+    }
 
-  endTrackScreen(params = {}) {
-    this._loop((x) => {
-      if (typeof x.endTrackScreen === 'function') {
-        x.endTrackScreen(PAGE_LOADED_EVENT, Object.assign({}, params));
-      }
-    });
-  }
+    endTrackScreen(params = {}) {
+        this._loop((x) => {
+            if (typeof x.endTrackScreen === 'function') {
+                x.endTrackScreen(PAGE_LOADED_EVENT, Object.assign({}, params));
+            }
+        });
+    }
 
-  logEvent(name, params = {}) {
-    this._loop((x) => {
-      if (typeof x.logEvent === 'function') {
-        x.logEvent(name, Object.assign({}, params));
-      }
-    });
-  }
+    logEvent(name, params = {}) {
+        this._loop((x) => {
+            if (typeof x.logEvent === 'function') {
+                x.logEvent(name, Object.assign({}, params));
+            }
+        });
+    }
 
-  endTimedEvent(name, params = {}) {
-    this._loop((x) => {
-      if (typeof x.endTimedEvent === 'function') {
-        x.endTimedEvent(name, Object.assign({}, params));
-      }
-    });
-  }
+    endTimedEvent(name, params = {}) {
+        this._loop((x) => {
+            if (typeof x.endTimedEvent === 'function') {
+                x.endTimedEvent(name, Object.assign({}, params));
+            }
+        });
+    }
 
-  _loop(callback) {
-    this.tools.map(callback);
-  }
+    _loop(callback) {
+        this.tools.map(callback);
+    }
 }
 
 export default new Analytics();
